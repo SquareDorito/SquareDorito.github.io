e360d5ec04552aee09df54be19ba623a1aed864f
kennoh <khnoh@brown.edu>
2019-03-24 14:24:54 -0700

    b

--- bmo/assets/js/ztips.js
+++ bmo/assets/js/ztips.js
@@ -1,16 +1,16 @@
 
 function populate_html(zips_dict) {
     for(var i=0;i<36;i++){
-        $('#tips-col1').append('<a href="#" class="button style2 ztip-button">'+zips_dict[i]['title']+'</a>')
+        $('#tips-col1').append('<a href="#" class="button style2 ztips">'+zips_dict[i]['title']+'</a>')
     }
     for(var i=36;i<72;i++){
-        $('#tips-col2').append('<a href="#" class="button style2 ztip-button">'+zips_dict[i]['title']+'</a>')
+        $('#tips-col2').append('<a href="#" class="button style2 ztips">'+zips_dict[i]['title']+'</a>')
     }
     for(var i=72;i<108;i++){
-        $('#tips-col3').append('<a href="#" class="button style2 ztip-button">'+zips_dict[i]['title']+'</a>')
+        $('#tips-col3').append('<a href="#" class="button style2 ztips">'+zips_dict[i]['title']+'</a>')
     }
     for(var i=108;i<142;i++){
-        $('#tips-col4').append('<a href="#" class="button style2 ztip-button">'+zips_dict[i]['title']+'</a>')
+        $('#tips-col4').append('<a href="#" class="button style2 ztips">'+zips_dict[i]['title']+'</a>')
     }
 }
 
