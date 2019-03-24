5c2dd1c1672192b9575b387de31f7ee2875fb389
kennoh <khnoh@brown.edu>
2019-03-24 14:02:43 -0700

    debugging ztip script

--- bmo/assets/js/ztips.js
+++ bmo/assets/js/ztips.js
@@ -1,14 +1,26 @@
-$.get('ztips.txt', function(data) {
-    var zips_dict = {}
-    data = data.split("\n");
-    n = data.length;
-    let counter=0;
-    for(var i=0;i<n;i+=3){
-        if(i%2==0){
-            zips_dict[counter]={"date":data[i],"title":data[i+1],"tip":data[i+2]}
-            counter++;
-        }
+
+function populate_html(zips_dict) {
+    for(var i=0;i<36;i++){
+        $('tips-col1').append('<p><a href="#">'+zips_dict[i]['title']+'</a></p>')
     }
-    console.log(counter);
-    console.log(zips_dict);
+}
+
+$(document).ready(function () {
+
+    $.get('ztips.txt', function (data) {
+        var zips_dict = {}
+        data = data.split("\n");
+        n = data.length;
+        let counter = 0;
+        for (var i = 0; i < n; i += 3) {
+            if (i % 2 == 0) {
+                zips_dict[counter] = { "date": data[i], "title": data[i + 1], "tip": data[i + 2] }
+                counter++;
+            }
+        }
+        console.log(counter);
+        console.log(zips_dict);
+        populate_html(zips_dict);
+    });
+
 });
