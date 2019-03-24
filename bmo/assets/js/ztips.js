df02ca5913847a56e69ffd3d6b9df90d6e013dd8
kennoh <khnoh@brown.edu>
2019-03-24 14:48:36 -0700

    three columns

--- bmo/assets/js/ztips.js
+++ bmo/assets/js/ztips.js
@@ -1,17 +1,14 @@
 
 function populate_html(zips_dict) {
     style_string = 'style="text-transform: none;font-weight:400;letter-spacing:0.015em;width:100%;text-align:center;"';
-    for(var i=0;i<36;i++){
+    for(var i=0;i<48;i++){
         $('#tips-col1').append('<a href="#" class="button style2 ztips" '+style_string+'>'+zips_dict[i]['title']+'</a>')
     }
-    for(var i=36;i<72;i++){
+    for(var i=48;i<96;i++){
         $('#tips-col2').append('<a href="#" class="button style2 ztips" '+style_string+'>'+zips_dict[i]['title']+'</a>')
     }
-    for(var i=72;i<108;i++){
+    for(var i=96;i<142;i++){
         $('#tips-col3').append('<a href="#" class="button style2 ztips" '+style_string+'>'+zips_dict[i]['title']+'</a>')
-    }
-    for(var i=108;i<142;i++){
-        $('#tips-col4').append('<a href="#" class="button style2 ztips" '+style_string+'>'+zips_dict[i]['title']+'</a>')
     }
 }
 
