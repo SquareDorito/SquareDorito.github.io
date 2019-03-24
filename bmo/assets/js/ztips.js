b3e59f2ec3996e6ea4a87479fabf87fa09246d48
kennoh <khnoh@brown.edu>
2019-03-24 13:15:25 -0700

    reading from txt file

--- bmo/assets/js/ztips.js
+++ bmo/assets/js/ztips.js
@@ -1,9 +1,12 @@
-$.get('ztips.txt', function(data) {    
-    var lines = data.split("\n");
-
-    $.each(lines, function(n, elem) {
-       //$('#myContainer').append('<div>' + elem + '</div>');
-       alert(elem);
-    });
-    
+$.get('ztips.txt', function(data) {
+    var zips_dict = {}
+    var n = data.split("\n");
+    let counter=0;
+    for(var i=0;i<n/3;i+=3){
+        if(i%2==0){
+            zips_dict[counter]={"date":data[i],"title":data[i+1],"tip":data[i+2]}
+            counter++;
+        }
+    }
+    console.log(zips_dict);
 });
